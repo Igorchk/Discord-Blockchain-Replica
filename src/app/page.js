f4877c3e7b26f110641c6e5fb902f252ea9f6b6f
dennis.rav <dennis826r@gmail.com>
2025-11-18 12:45:26 -0800

    Initial Setup. .gitignore and base homepage page.js

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+"use client";
+import {useState} from "react";
+
+export default function HomePage() {
+//
+
+    return (
+        <html>
+        <body>
+            <main className="min-h-screen">
+                <section className="mx-auto max-w-3xl p-6 bg-green">
+                    <h2 className="font-bold">Test</h2>
+                </section>
+            </main>
+        </body>
+        </html>
+    );
+}
